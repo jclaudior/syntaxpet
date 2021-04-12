2819b8ba57ac3cdafdf586b646b4a70e877dff8b
jocribeiro <jocribeiro@rd.com.br>
2021-04-12 20:48:38 -0300

    adicionado acessorios

--- js/script.js
+++ js/script.js
@@ -1,113 +1,165 @@
-const slider = document.getElementById('alimentacao');
-let isDown = false;
-let startX;
-let scrollLeft;
+const sliderAlimentacao = document.getElementById('alimentacao');
+let isDownAlimentacao = false;
+let startXAlimentacao;
+let scrollLeftAlimentacao;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+sliderAlimentacao.addEventListener('mousedown', (e) => {
+  isDownAlimentaca = true;
+  sliderAlimentacao.classList.add('active');
+  startXAlimentacao = e.pageX - sliderAlimentacao.offsetLeft;
+  scrollLeftAlimentacao = sliderAlimentacao.scrollLeft;
 });
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+sliderAlimentacao.addEventListener('mouseleave', () => {
+  isDownAlimentaca = false;
+  sliderAlimentacao.classList.remove('active');
 });
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
+sliderAlimentacao.addEventListener('mouseup', () => {
+  isDownAlimentaca = false;
+  sliderAlimentacao.classList.remove('active');
 });
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
+sliderAlimentacao.addEventListener('mousemove', (e) => {
+  if(!isDownAlimentaca) return;
   e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
+  const x = e.pageX - sliderAlimentacao.offsetLeft;
+  const walk = (x - startXAlimentacao) * 3; //scroll-fast
+  sliderAlimentacao.scrollLeft = scrollLeftAlimentacao - walk;
 });
 
 
-const slider2 = document.getElementById('alimentacaoDesc');
-let isDown2 = false;
-let startY;
-let scrollTop;
+const sliderAlimentacaoDesc = document.getElementById('alimentacaoDesc');
+let isDownAlimentacaoDesc = false;
+let startYAlimentacaoDescAlimentacaoDesc;
+let scrollTopAlimentacaoDesc;
 
-slider2.addEventListener('mousedown', (e) => {
-  isDown2 = true;
-  slider2.classList.add('active');
-  startY = e.pageY - slider2.offsetTop;
-  scrollTop = slider2.scrollTop;
+sliderAlimentacaoDesc.addEventListener('mousedown', (e) => {
+  isDownAlimentacaoDesc = true;
+  sliderAlimentacaoDesc.classList.add('active');
+  startYAlimentacaoDesc = e.pageY - sliderAlimentacaoDesc.offsetTop;
+  scrollTopAlimentacaoDesc = sliderAlimentacaoDesc.scrollTop;
 });
-slider2.addEventListener('mouseleave', () => {
-  isDown2 = false;
-  slider2.classList.remove('active');
+sliderAlimentacaoDesc.addEventListener('mouseleave', () => {
+  isDownAlimentacaoDesc = false;
+  sliderAlimentacaoDesc.classList.remove('active');
 });
-slider2.addEventListener('mouseup', () => {
-  isDown2 = false;
-  slider2.classList.remove('active');
+sliderAlimentacaoDesc.addEventListener('mouseup', () => {
+  isDownAlimentacaoDesc = false;
+  sliderAlimentacaoDesc.classList.remove('active');
 });
-slider2.addEventListener('mousemove', (e) => {
-  if(!isDown2) return;
+sliderAlimentacaoDesc.addEventListener('mousemove', (e) => {
+  if(!isDownAlimentacaoDesc) return;
   e.preventDefault();
-  const y = e.pageY - slider2.offsetTop;
-  const walk = (y - startY) * 3; //scroll-fast
-  slider2.scrollTop = scrollTop - walk;
-  console.log(walk);
+  const y = e.pageY - sliderAlimentacaoDesc.offsetTop;
+  const walk = (y - startYAlimentacaoDesc) * 3; //scroll-fast
+  sliderAlimentacaoDesc.scrollTop = scrollTopAlimentacaoDesc - walk;
 });
 
-const slider3 = document.getElementById('brinquedos');
-let isDown3 = false;
-let startX3;
-let scrollLeft3;
+const sliderBrinquedos = document.getElementById('brinquedos');
+let isDownBrinquedos = false;
+let startXBrinquedos;
+let scrollLeftBrinquedos;
 
-slider3.addEventListener('mousedown', (e) => {
-  isDown3 = true;
-  slider3.classList.add('active');
-  startX3 = e.pageX - slider3.offsetLeft;
-  scrollLeft3 = slider3.scrollLeft;
+sliderBrinquedos.addEventListener('mousedown', (e) => {
+  isDownBrinquedos = true;
+  sliderBrinquedos.classList.add('active');
+  startXBrinquedos = e.pageX - sliderBrinquedos.offsetLeft;
+  scrollLeftBrinquedos = sliderBrinquedos.scrollLeft;
 });
-slider3.addEventListener('mouseleave', () => {
-  isDown3 = false;
-  slider3.classList.remove('active');
+sliderBrinquedos.addEventListener('mouseleave', () => {
+  isDownBrinquedos = false;
+  sliderBrinquedos.classList.remove('active');
 });
-slider3.addEventListener('mouseup', () => {
-  isDown3 = false;
-  slider3.classList.remove('active');
+sliderBrinquedos.addEventListener('mouseup', () => {
+  isDownBrinquedos = false;
+  sliderBrinquedos.classList.remove('active');
 });
-slider3.addEventListener('mousemove', (e) => {
-  if(!isDown3) return;
+sliderBrinquedos.addEventListener('mousemove', (e) => {
+  if(!isDownBrinquedos) return;
   e.preventDefault();
-  const x = e.pageX - slider3.offsetLeft;
-  const walk = (x - startX3) * 3; //scroll-fast
-  slider3.scrollLeft = scrollLeft3 - walk;
-  console.log(walk);
+  const x = e.pageX - sliderBrinquedos.offsetLeft;
+  const walk = (x - startXBrinquedos) * 3; //scroll-fast
+  sliderBrinquedos.scrollLeft = scrollLeftBrinquedos - walk;
 });
 
 
-const slider4 = document.getElementById('brinquedosDesc');
-let isDown4 = false;
-let startY4;
-let scrollTop4;
+const sliderBrinquedosDesc = document.getElementById('brinquedosDesc');
+let isDownBrinquedosDesc = false;
+let startYBrinquedosDesc;
+let scrollTopBrinquedosDesc;
 
-slider4.addEventListener('mousedown', (e) => {
-  isDown4 = true;
-  slider4.classList.add('active');
-  startY4 = e.pageY - slider4.offsetTop;
-  scrollTop4 = slider4.scrollTop;
+sliderBrinquedosDesc.addEventListener('mousedown', (e) => {
+  isDownBrinquedosDesc = true;
+  sliderBrinquedosDesc.classList.add('active');
+  startYBrinquedosDesc = e.pageY - sliderBrinquedosDesc.offsetTop;
+  scrollTopBrinquedosDesc = sliderBrinquedosDesc.scrollTop;
 });
-slider4.addEventListener('mouseleave', () => {
-  isDown4 = false;
-  slider4.classList.remove('active');
+sliderBrinquedosDesc.addEventListener('mouseleave', () => {
+  isDownBrinquedosDesc = false;
+  sliderBrinquedosDesc.classList.remove('active');
 });
-slider4.addEventListener('mouseup', () => {
-  isDown4 = false;
-  slider4.classList.remove('active');
+sliderBrinquedosDesc.addEventListener('mouseup', () => {
+  isDownBrinquedosDesc = false;
+  sliderBrinquedosDesc.classList.remove('active');
 });
-slider4.addEventListener('mousemove', (e) => {
-  if(!isDown4) return;
+sliderBrinquedosDesc.addEventListener('mousemove', (e) => {
+  if(!isDownBrinquedosDesc) return;
   e.preventDefault();
-  const y = e.pageY - slider4.offsetTop;
-  const walk = (y - startY4) * 3; //scroll-fast
-  slider4.scrollTop = scrollTop4 - walk;
-  console.log(walk);
+  const y = e.pageY - sliderBrinquedosDesc.offsetTop;
+  const walk = (y - startYBrinquedosDesc) * 3; //scroll-fast
+  sliderBrinquedosDesc.scrollTop = scrollTopBrinquedosDesc - walk;
+});
+
+
+const sliderAcessorios = document.getElementById('acessorios');
+let isDownAcessorios = false;
+let startXAcessorios;
+let scrollLeftAcessorios;
+
+sliderAcessorios.addEventListener('mousedown', (e) => {
+  isDownAcessorios = true;
+  sliderAcessorios.classList.add('active');
+  startXAcessorios = e.pageX - sliderAcessorios.offsetLeft;
+  scrollLeftAcessorios = sliderAcessorios.scrollLeft;
+});
+sliderAcessorios.addEventListener('mouseleave', () => {
+  isDownAcessorios = false;
+  sliderAcessorios.classList.remove('active');
+});
+sliderAcessorios.addEventListener('mouseup', () => {
+  isDownAcessorios = false;
+  sliderAcessorios.classList.remove('active');
+});
+sliderAcessorios.addEventListener('mousemove', (e) => {
+  if(!isDownAcessorios) return;
+  e.preventDefault();
+  const x = e.pageX - sliderAcessorios.offsetLeft;
+  const walk = (x - startXAcessorios) * 3; //scroll-fast
+  sliderAcessorios.scrollLeft = scrollLeftAcessorios - walk;
+});
+
+
+const sliderAcessoriosDesc = document.getElementById('acessoriosDesc');
+let isDownAcessoriosDesc = false;
+let startYAcessoriosDesc;
+let scrollTopAcessoriosDesc;
+
+sliderAcessoriosDesc.addEventListener('mousedown', (e) => {
+  isDownAcessoriosDesc = true;
+  sliderAcessoriosDesc.classList.add('active');
+  startYAcessoriosDesc = e.pageY - sliderAcessoriosDesc.offsetTop;
+  scrollTopAcessoriosDesc = sliderAcessoriosDesc.scrollTop;
+});
+sliderAcessoriosDesc.addEventListener('mouseleave', () => {
+  isDownAcessoriosDesc = false;
+  sliderAcessoriosDesc.classList.remove('active');
+});
+sliderAcessoriosDesc.addEventListener('mouseup', () => {
+  isDownAcessoriosDesc = false;
+  sliderAcessoriosDesc.classList.remove('active');
+});
+sliderAcessoriosDesc.addEventListener('mousemove', (e) => {
+  if(!isDownAcessoriosDesc) return;
+  e.preventDefault();
+  const y = e.pageY - sliderAcessoriosDesc.offsetTop;
+  const walk = (y - startYAcessoriosDesc) * 3; //scroll-fast
+  sliderAcessoriosDesc.scrollTop = scrollTopAcessoriosDesc - walk;
 });
