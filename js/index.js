938a5951f00243107902904cd007576524e716ae
Fabiano Guimarães <fabio.dub@outlook.com>
2021-05-17 21:56:52 -0300

    links para pagina de serviços

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ var counter = 1;
             counter = 1;
         }
     }, 5000);
+
+    
+const links = document.querySelectorAll('.cardserv a')
+
+links.forEach(link => link.addEventListener('click', navegar))
+
+function navegar(e) {
+    const href = e.target.parentElement.getAttribute('href')
+    const path = href.split('.')
+
+    window.location.href =  `${path[1]}.${path[2]}`
+}
